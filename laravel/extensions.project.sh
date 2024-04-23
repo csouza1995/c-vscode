c313c3e8de8631489050252e5aadc8d3478d3031
Carlos Souza <51545352+csouza1995@users.noreply.github.com>
2024-04-23 13:57:24 -0300

    Create extensions.project.sh for laravel

---
+++ laravel/extensions.project.sh
@@ -0,0 +1,21 @@
+# php support
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension junstyle.php-cs-fixer
+code --install-extension MehediDracula.php-namespace-resolver
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension liamhammett.inline-parameters
+
+# php unit testing
+code --install-extension m1guelpf.better-pest
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension recca0120.vscode-phpunit
+
+# laravel support
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+code --install-extension austenc.laravel-blade-spacer
+code --install-extension cjhowe7.laravel-blade
+code --install-extension codingyu.laravel-goto-view
+code --install-extension ryannaddy.laravel-artisan
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension mikestead.dotenv
+code --install-extension bradlc.vscode-tailwindcss
