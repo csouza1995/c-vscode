09a436e7ec55e724ab0246e939d5e0198f5118a6
Carlos Souza <51545352+csouza1995@users.noreply.github.com>
2024-04-23 13:56:52 -0300

    Create extensions.user.sh

---
+++ extensions.user.sh
@@ -0,0 +1,26 @@
+# remote ssh and wsl
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.remote-explorer
+
+# docker
+code --install-extension ms-azuretools.vscode-docker
+
+# ui and styles
+code --install-extension drcika.apc-extension
+code --install-extension miguelsolorio.min-theme
+code --install-extension PKief.material-icon-theme
+
+# code quality
+code --install-extension streetsidesoftware.code-spell-checker
+
+# git and assistant
+code --install-extension GitHub.copilot
+code --install-extension mhutchie.git-graph
+
+# project management
+code --install-extension alefragnani.project-manager
+
+# time tracking
+code --install-extension WakaTime.vscode-wakatime
